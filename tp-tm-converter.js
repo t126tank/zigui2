c11a34177afba7dbaade2b9dc3a15da1f9fd87cd
t126tank <t126tank@users.noreply.github.com>
2016-09-21 20:15:46 +0900

    Update tp-tm-converter.js

--- tp-tm-converter.js
+++ tp-tm-converter.js
@@ -31,7 +31,20 @@ function weekStr(wk) {
    return getLength(wk) == 2 ? "0" + wk.toString(): "00" + wk.toString();
 };
 
-function validTm(t, p) {
+function getDateOfWeek(weekNumber, year){
+   if (weekNumber < 1) {
+      throw createError('Week num is less than 1', 'EINVAL');
+   }
+   // Create a date object starting january first of chosen year, plus the number of days in a week multiplied by the week number to get the right date.
+   return new Date(year, 0,  1 + ((weekNumber - 1) * 7));
+};
+
+function tm2WeekNumber(tm) {
+   var onejan = new Date(tm.getFullYear(), 0, 1);
+   return Math.ceil((((tm - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+};
+
+function validTp(tp, period) {
    /**
     * true  - 20160112, 2015033, 201411
     * false - 11112233, 2013069, 201213
@@ -51,26 +64,28 @@ TpTmConverter.mapTpTm = function() {
 TpTmConverter.tp2Tm = function(tp) {
    var tm = new Date();
    var period = TpTmConverter.judgePeriod(tp);
-   var str = tp.toString();
+   var tpStr = tp.toString();
 
-   tm.setFullYear(parseInt(str.substr(0, 4)));
+   tm.setFullYear(parseInt(tpStr.substr(0, 4)));
    if (period != periods.YEARLY) {
       if (period != periods.WEEKLY) {
-         var mon = parseInt(str.substr(4, 2)) - 1;
+         var mon = parseInt(tpStr.substr(4, 2)) - 1;
          tm.setMonth(mon);
          if (period != periods.MONTHLY) {
-            tm.setDate(parseInt(str.substr(6, 2)));
+            tm.setDate(parseInt(tpStr.substr(6, 2)));
             if ((period != periods.DAILY)) {
-               tm.setHours(parseInt(str.substr(8, 2)));
+               tm.setHours(parseInt(tpStr.substr(8, 2)));
                if ((period != periods.HOURLY)) {
-                  tm.setMinutes(parseInt(str.substr(10, 2)));
+                  tm.setMinutes(parseInt(tpStr.substr(10, 2)));
                   if ((period != periods.MINUTE)) {
-                     tm.setSeconds(parseInt(str.substr(12, 2)));
+                     tm.setSeconds(parseInt(tpStr.substr(12, 2)));
                   }
                }
             }
          }
-      } else { // WEEKLY
+      } else { // periods.WEEKLY
+         var wk = parseInt(tpStr.substr(4, 3));
+         tm = getDateOfWeek(wk, tm.getFullYear());
       }
    }
 
@@ -78,30 +93,30 @@ TpTmConverter.tp2Tm = function(tp) {
 };
 
 TpTmConverter.tm2Tp = function(tm, p) {
-   var tp = tm.getFullYear().toString();
+   var tpStr = tm.getFullYear().toString();
 
    if (p != periods.YEARLY) {
       if (p != periods.WEEKLY) {
          var mon = tm.getMonth() + 1;
-         tp += timeStr(mon);
+         tpStr += timeStr(mon);
          if (p != periods.MONTHLY) {
-            tp += timeStr(tm.getDate());
+            tpStr += timeStr(tm.getDate());
             if ((p != periods.DAILY)) {
-               tp += timeStr(tm.getHours());
+               tpStr += timeStr(tm.getHours());
                if ((p != periods.HOURLY)) {
-                  tp += timeStr(tm.getMinutes());
+                  tpStr += timeStr(tm.getMinutes());
                   if ((p != periods.MINUTE)) {
-                     tp += timeStr(tm.getSeconds());
+                     tpStr += timeStr(tm.getSeconds());
                   }
                }
             }
          }
-      } else { // WEEKLY
-         tp += weekStr();
+      } else { // periods.WEEKLY
+         tpStr += weekStr(tm2WeekNumber(tm));
       }
    }
 
-   return tp;
+   return parseInt(tpStr);
 };
 
 TpTmConverter.initTpTmMap = function(s, e) {
@@ -113,17 +128,19 @@ TpTmConverter.initTpTmMap = function(s, e) {
    tmStart = s;
 };
 
-TpTmConverter.judgePeriod = function (tm) {
+TpTmConverter.judgePeriod = function (tp) {
    var period;
 
-   if (!Number.isInteger(tm)) {
+   // uint32 -> Max: 2,147,483,648
+   // uint64 -> Max: 9,223,372,036,854,775,808
+   if (!Number.isInteger(tp)) {
       throw createError('Start time is not integer', 'EINVAL');
    }
-   if (tm < 0) {
+   if (tp < 0) {
       throw createError('Start time is minus', 'EINVAL');
    }
 
-   switch (getLength(tm)) {
+   switch (getLength(tp)) {
    case immutableLen.second:
       period = periods.SECOND;
       break;
@@ -149,8 +166,8 @@ TpTmConverter.judgePeriod = function (tm) {
       throw createError('Period not supported', 'EINVAL');
    }
 
-   if (!validTm(tm, period)) {
-      throw createError('Start time is invalid', 'EINVAL');
+   if (!validTp(tp, period)) {
+      throw createError('Start trade point is invalid', 'EINVAL');
    }
 
    return period;
