d04158a5816e9eec0607578449cdd816a9ffc43d
t126tank <t126tank@users.noreply.github.com>
2016-10-12 16:16:48 +0900

    Update server.php

--- php/server.php
+++ php/server.php
@@ -1,1 +1,18 @@
+<?php
 
+$postJson = json_encode($_POST);
+# $postJson = $_POST['myData'];
+// echo $postJson;
+echo json_encode($_POST);
+
+$jsonObj = array(
+	'name' => 'Json',
+	'gender' => 'Male',
+	'age' => 35,
+	'time' => date('Y:m:d g:i:s')
+);
+
+$jsonStr = json_encode($jsonObj);
+
+// echo $jsonStr;
+?>
