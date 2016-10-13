e8874f17e0d688c086279c6b46a872a3ec678367
t126tank <t126tank@users.noreply.github.com>
2016-10-13 15:49:33 +0900

    Update login_check.php

--- php/login/login_check.php
+++ php/login/login_check.php
@@ -1,43 +1,39 @@
 <?php
+require ("mysql_pdo/Db.class.php");
+require_once ("dbg/dbg.php");
 
-$host="localhost"; // Host name 
-$username=""; // Mysql username 
-$password=""; // Mysql password 
-$db_name="test"; // Database name 
-$tbl_name="members"; // Table name 
-
-// Connect to server and select databse.
-mysql_connect("$host", "$username", "$password") or die("cannot connect"); 
-mysql_select_db("$db_name") or die("cannot select DB");
-
+if (!isset($_POST['myusername']) || !isset($_POST['mypassword'])) {
+    header("location:login.html");
+}
 // username and password sent from form
 // Never sent unchecked data to mysql server
 // Creating md5 hashed to prevent from mysql injections
-$myusername=md5(strtolower($_POST['username']));
-$mypassword=md5($_POST['password']);
+$myusername=md5(strtolower($_POST['myusername']));
+$mypassword=md5($_POST['mypassword']);
 
 // To protect MySQL injection (more detail about MySQL injection)
 $myusername = stripslashes($myusername);
 $mypassword = stripslashes($mypassword);
-$myusername = mysql_real_escape_string($myusername);
-$mypassword = mysql_real_escape_string($mypassword);
 
-$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
-$result=mysql_query($sql);
+$db = new Db();
 
-// Mysql_num_row is counting table row
-$count=mysql_num_rows($result);
+// $myusername = mysql_real_escape_string($myusername); PDO::quote()
+// $mypassword = mysql_real_escape_string($mypassword); PDO::quote()
+
+$db->bindMore(array("email"=>$myusername, "password"=>$mypassword));
+$result = $db->single("SELECT * FROM persons WHERE Email = :email AND Password = :password");
+// $person_count = array_values($person_count);
 
 // If result matched $myusername and $mypassword, table row must be 1 row
-if ($count==1) {
+if ($result != null) {
+  session_start();
   // Register $myusername, $mypassword and redirect to file "login_success.php"
-  session_register("myusername");
-  session_register("mypassword");
+  $_SESSION['myusername'] = true;
+  $_SESSION['mypassword'] = true;
   $_SESSION['is_logged_in'] = true;
   $_SESSION['expires'] = time() + 3600;   // 3600 seconds session lifetime
   header("location:login_success.php");
-}
-else {
+} else {
   echo "Wrong Username or Password";
   // Redirect to re-login
 }
