cc629d28448b8135eafeb21feafc1ffa0bd03baa
t126tank <t126tank@users.noreply.github.com>
2016-10-19 15:22:48 +0900

    Update login_success.php

--- php/login/login_success.php
+++ php/login/login_success.php
@@ -11,11 +11,76 @@ if (!$_SESSION['myusername'] || !$_SESSION['myusername'] ||
   header("location:login.html"); // Redirect to re-login
 } else {
   $_SESSION['expires'] = time() + 3600; // refresh the lifetime
+  echo "Login Successful ".$_SESSION['myusername']."<br>";
 }
 ?>
 
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<!-- JS -->
+<script type="text/javascript" src="./js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript">
+<!--
+
+$(document).ready(function() {
+    function refresh() {
+        var url = "./doRefresh.php";
+
+        $.ajax({
+            type: "post",
+            url: url,
+            // dataType: "json",
+            // data: "",
+            success: function (msg) {
+                var backdata = msg;
+                $("#backdata").html(backdata);
+                $("#backdata").css({color: "green"});
+                // alert('success!');
+
+                $('.hoge tr').addClass('hv');
+
+	            $('.hoge tr').hover(
+		            function() {
+			            //hoverクラス「hv」を追加する
+			            $(this).addClass('hv');
+		            },
+		            function() {
+		            //マウスアウトしたら
+			            //hoverクラス「hv」を削除する
+			            $(this).removeClass('hv');
+		            }
+	            );
+	            setTimeout (refresh, 100);
+            },
+            error: function(e) {
+                console.log(e.message);
+            }
+        });
+
+        // alert('Hi');
+    }
+    setTimeout (refresh, 20);
+});
+
+-->
+</script>
+
+<!-- CSS -->
+<style type="text/css">
+    table.hoge tr.hv td {
+        background-color: #fff4f8;
+    }
+</style>
+
+
+</head>
+
 <body>
-Login Successful
+echo "hello";
+
+<span id="backdata"></span>
+
+<br>
+<a href="logout.php"> Logout </a>
 </body>
 </html>
