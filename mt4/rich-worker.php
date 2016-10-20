255d2d70bfc35a94ddbc44b7937f621e15b4fb90
t126tank <t126tank@users.noreply.github.com>
2016-10-20 17:58:46 +0900

    Update rich-worker.php

--- mt4/rich-worker.php
+++ mt4/rich-worker.php
@@ -3,27 +3,27 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
+
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
 $channel->queue_declare('hello', false, false, false, false);
 
-echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+// echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
 
 $callback = function($msg) {
-  echo " [x] Received ", $msg->body, "\n";
+  echo $msg->body; // offer inverted orders to richMT4
 };
 
 $channel->basic_consume('hello', '', false, true, false, false, $callback);
 
-
-// while(count($channel->callbacks)) {
-if (!count($channel->callbacks)) {
-    $channel->wait();
+echo "9999999999999999999999";
+$timeout = 1;
+while(count($channel->callbacks)) {
+    $channel->wait(null, false, $timeout);
 }
 
 $channel->close();
 $connection->close();
-
 
 ?>
