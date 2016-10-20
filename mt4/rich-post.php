ec50a153b68fe617589b74754f6e9924aa7a8a2a
t126tank <t126tank@users.noreply.github.com>
2016-10-20 17:29:07 +0900

    Create rich-post.php

---
+++ mt4/rich-post.php
@@ -0,0 +1,37 @@
+<?php
+
+$jsonObj = file_get_contents('php://input');
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$msg = new AMQPMessage("rich-post.php");
+// $msg = new AMQPMessage(invertJsonObj($jsonObj));
+$channel->basic_publish($msg, '', 'hello');
+
+$channel->close();
+$connection->close();
+
+function invertJsonObj($obj) {
+    $ordArray = json_decode($obj, TRUE);
+
+    array_walk($ordArray, 'cb');
+
+    return json_encode($ordArray);
+}
+
+function cb(&$item) {
+    if ($item['type'] == 'buy')
+        $item['type'] = 'sell';
+    else
+        $item['type'] = 'buy';
+};
+
+?>
