21ffd1291b982b4cb9ecb7081a793251f540fac6
t126tank <t126tank@users.noreply.github.com>
2016-10-20 15:07:28 +0900

    Update mt4poor.php

--- mt4/dummy/mt4poor.php
+++ mt4/dummy/mt4poor.php
@@ -3,32 +3,54 @@ require_once('./json-post.php');
 
 $time = date("Y:m:d g:i:s");
 
+$data = array(
+         array(
+            "time" => $time,
+            "ticket" => 666,
+            "price" => 888.88,
+            "type" => "buy"
+          ),
+         array(
+            "time" => $time,
+            "ticket" => 888,
+            "price" => 666.66,
+            "type" => "sell"
+         ),
+         array(
+            "time" => $time,
+            "ticket" => 686,
+            "price" => 222.22,
+            "type" => "buy")
+);
+$jsonObj = json_encode($data);
+
+/*
 $jsonStr = '[
    {
       "time": ' . $time . ',
       "ticket": 666,
       "price": 888.88,
-      "type" => "buy"
+      "type" : "buy"
    },
+   {
       "time": ' . $time . ',
       "ticket": 888,
       "price": 666.66,
-      "type" => "sell"
-   {
+      "type": "sell"
    },
    {
       "time": ' . $time . ',
       "ticket": 686,
       "price": 222.22,
-      "type" => "buy"
+      "type": "buy"
    }
 ]';
-
 $jsonObj = json_encode($jsonStr);
+*/
+
+
 $link = 'http://localhost/pqs/mt4/poor-post.php';
 
-postFromHTTP($link, $jsonObj);
-
-// echo $jsonObj;
+echo postFromHTTP($link, $jsonObj);
 
 ?>
