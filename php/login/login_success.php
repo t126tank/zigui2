b6f36d14104719fe4cf7828ace8a915c5992ad8c
t126tank <t126tank@users.noreply.github.com>
2016-10-21 13:46:45 +0900

    Update login_success.php

--- php/login/login_success.php
+++ php/login/login_success.php
@@ -1,17 +1,18 @@
 <?php
 session_start();
 
+require ("mysql_pdo/Db.class.php");
 require_once ("dbg/dbg.php");
 
-if (!$_SESSION['myusername'] || !$_SESSION['myusername'] ||
+if (!$_SESSION['mypassword'] ||
     !$_SESSION['is_logged_in'] || $_SESSION['expires'] < time()) {
-  session_unset();
-  session_destroy();
-  // exit;
-  header("location:login.html"); // Redirect to re-login
+    session_unset();
+    session_destroy();
+    // exit;
+    header("location:login.html"); // Redirect to re-login
 } else {
-  $_SESSION['expires'] = time() + 3600; // refresh the lifetime
-  echo "Login Successful ".$_SESSION['myusername']."<br>";
+    $_SESSION['expires'] = time() + 3600; // refresh the lifetime
+    echo "Login Successful ".$_SESSION['myusername']."<br>";
 }
 ?>
 
@@ -37,7 +38,7 @@ $(document).ready(function() {
                 $("#backdata").css({color: "green"});
                 // alert('success!');
 
-                $('.hoge tr').addClass('hv');
+                // $('.hoge tr').addClass('hv');
 
 	            $('.hoge tr').hover(
 		            function() {
@@ -50,7 +51,7 @@ $(document).ready(function() {
 			            $(this).removeClass('hv');
 		            }
 	            );
-	            setTimeout (refresh, 100);
+	            setTimeout (refresh, 1500);
             },
             error: function(e) {
                 console.log(e.message);
@@ -59,7 +60,8 @@ $(document).ready(function() {
 
         // alert('Hi');
     }
-    setTimeout (refresh, 20);
+    // setTimeout (refresh, 20);
+    refresh();
 });
 
 -->
@@ -78,9 +80,13 @@ $(document).ready(function() {
 <body>
 echo "hello";
 
-<span id="backdata"></span>
-
 <br>
 <a href="logout.php"> Logout </a>
+<br>
+
+
+<span id="backdata"></span>
+
+
 </body>
 </html>
