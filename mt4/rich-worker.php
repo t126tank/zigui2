233c794486a90b54e266998998847de8a3b74ca5
t126tank <t126tank@users.noreply.github.com>
2016-10-24 14:10:48 +0900

    Update rich-worker.php

--- mt4/rich-worker.php
+++ mt4/rich-worker.php
@@ -1,29 +1,39 @@
 <?php
+include(__DIR__ . '/config.php');
 
-require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Exception\AMQPTimeoutException;
 
-
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection(HOST, PORT, USER, PASS, VHOST);
 $channel = $connection->channel();
+$channel->queue_declare(TCH, false, false, false, false);
 
-$channel->queue_declare('hello', false, false, false, false);
+$tradeData = '[';
 
 // echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
-
 $callback = function($msg) {
-  echo $msg->body; // offer inverted orders to richMT4
+  $tradeData .= $msg->body ;  // offer inverted orders to richMT4
+  $tradeData .= ',';
 };
 
-$channel->basic_consume('hello', '', false, true, false, false, $callback);
+$channel->basic_consume(TCH, '', false, true, false, false, $callback);
 
-echo "9999999999999999999999";
 $timeout = 1;
-while(count($channel->callbacks)) {
-    $channel->wait(null, false, $timeout);
+while (count($channel->callbacks)) {
+    try {
+        $channel->wait(null, false, $timeout); // ($allowed_methods=null, $non_blocking = false, $timeout = 0)
+    } catch (PhpAmqpLib\Exception\AMQPTimeoutException $e) {
+        $channel->close();
+        $connection->close();
+
+        $tradeData  = substr($tradeData, 0, -1);
+        $tradeData .= ']';
+        echo $tradeData;
+
+        exit;
+    }
 }
 
 $channel->close();
 $connection->close();
-
 ?>
