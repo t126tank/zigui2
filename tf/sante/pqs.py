dfb38faef6ebd741fd28ed4ac7b8966747e33f4b
t126tank <t126tank@users.noreply.github.com>
2017-01-19 20:46:57 +0900

    Update pqs.py

--- tf/sante/pqs.py
+++ tf/sante/pqs.py
@@ -22,18 +22,18 @@ csvTest = np.genfromtxt(IRIS_TEST, delimiter=",", skip_header=1)
 X_test = np.array(csvTest[:, :49])
 y_test = csvTest[:,49]
 
-X_train = X_train.reshape([-1,7,7,1])
-X_test = X_test.reshape([-1,7,7,1])
+# X_train = X_train.reshape([-1,7,7,1])
+# X_test = X_test.reshape([-1,7,7,1])
 
 # reshape Y and Y_test to have shape (batch_size, 1).
 # y_train = y_train.reshape([-1, 28, 28, 1])
 # y_test = y_test.reshape([-1, 28, 28, 1])
 
-X_val=[]
-X_val=np.array(X_val)
+# X_val=[]
+# X_val=np.array(X_val)
 
-y_val=[]
-y_val=np.array(y_val)
+# y_val=[]
+# y_val=np.array(y_val)
 
 
 ### Ori ###
@@ -84,12 +84,28 @@ tl.layers.initialize_global_variables(sess)
 # train the network
 # http://tensorlayer.readthedocs.io/en/latest/modules/utils.html
 tl.utils.fit(sess, network, train_op, cost, X_train, y_train, x, y_,
-            acc=acc, batch_size=49, n_epoch=500, print_freq=5,
-            X_val=X_val, y_val=y_val, eval_train=False)
+            acc=acc, batch_size=25, n_epoch=5, print_freq=5,
+            eval_train=False)
+#           X_val=X_val, y_val=y_val, eval_train=False)
 
 # evaluation
 tl.utils.test(sess, network, acc, X_test, y_test, x, y_, batch_size=None, cost=cost)
 
 # save the network to .npz file
 tl.files.save_npz(network.all_params , name='model.npz')
+
+# predict
+
+new_samples = np.array(
+   [
+[1492.4, 1477.6, 1461.4, 1442.3, 1442.6, 1458.8, 1465.1, 1476.8, 1504.0, 1513.1, 1510.6, 1515.4, 1516.2, 1501.7, 1491.7, 1488.0, 1472.3, 1429.9, 1418.3, 1401.0, 1384.3, 1369.5, 1343.6, 1312.9, 1294.8, 1292.6, 1296.6, 1306.5, 1330.9, 1353.3, 1371.1, 1384.6, 1395.0, 1401.9, 1410.5, 1415.3, 1415.2, 1413.8, 1416.8, 1420.6, 1427.3, 1438.5, 1458.9, 1469.5, 1470.7, 1470.3, 1466.6, 1448.7, 1422.2] ,
+[1514.1, 1492.4, 1477.6, 1461.4, 1442.3, 1442.6, 1458.8, 1465.1, 1476.8, 1504.0, 1513.1, 1510.6, 1515.4, 1516.2, 1501.7, 1491.7, 1488.0, 1472.3, 1429.9, 1418.3, 1401.0, 1384.3, 1369.5, 1343.6, 1312.9, 1294.8, 1292.6, 1296.6, 1306.5, 1330.9, 1353.3, 1371.1, 1384.6, 1395.0, 1401.9, 1410.5, 1415.3, 1415.2, 1413.8, 1416.8, 1420.6, 1427.3, 1438.5, 1458.9, 1469.5, 1470.7, 1470.3, 1466.6, 1448.7] ,
+[1518.6, 1514.1, 1492.4, 1477.6, 1461.4, 1442.3, 1442.6, 1458.8, 1465.1, 1476.8, 1504.0, 1513.1, 1510.6, 1515.4, 1516.2, 1501.7, 1491.7, 1488.0, 1472.3, 1429.9, 1418.3, 1401.0, 1384.3, 1369.5, 1343.6, 1312.9, 1294.8, 1292.6, 1296.6, 1306.5, 1330.9, 1353.3, 1371.1, 1384.6, 1395.0, 1401.9, 1410.5, 1415.3, 1415.2, 1413.8, 1416.8, 1420.6, 1427.3, 1438.5, 1458.9, 1469.5, 1470.7, 1470.3, 1466.6] 
+   ],
+   dtype=float)
+
+y = network.outputs
+y_op = tf.argmax(tf.nn.softmax(y), 1)
+print(tl.utils.predict(sess, network, new_samples, x, y_op))
+
 sess.close()
