c43bfdb34ca4bf7654375c97488d574ee6b7fb74
t126tank <t126tank@users.noreply.github.com>
2017-01-23 16:04:54 +0900

    Add files via upload

--- tf/sante/start.sh
+++ tf/sante/start.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-sym="1570"
+sym="stocks/1570"
 if [[ ! -z "$1" ]] ; then
    sym=$1
 fi
 
+python readCsv2.py $sym
 
-pushd ..
 
 python writeCsv2.py $sym > $sym/out/new.txt
 rm -rf /tmp/iris_model/
 
 python tfCsv2.py $sym
 
-pushd $sym/out
-cp -f iris_test.csv iris_training.csv  ../../iris/
-popd
-
-popd
-
+pushd iris
+cp -f ../$sym/out/iris_test.csv ../$sym/out/iris_training.csv  .
 python pqs.py
+popd
+
 
