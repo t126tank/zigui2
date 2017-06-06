9dcccafe356f604ff79885df24f9eed12da2a470
t126tank <t126tank@users.noreply.github.com>
2017-06-06 11:21:01 +0900

    Add files via upload

---
+++ hedge/queryPositions.php
@@ -0,0 +1,58 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require_once __DIR__ . '/RedisDao.php';
+
+ob_start();
+
+$req = array('userId'=>'aaa');
+
+if (!isset($_GET['userId'])) {
+    echo "_POST problem";
+}
+$req['userId'] = $_GET['userId'];
+
+function calPosition($v) {
+    return $v['bullVol']*$v['bullPrice'] +
+           $v['bearVol']*$v['bearPrice'] +
+           $v['cash'];
+}
+
+$dao = new RedisDao();
+
+/* Get all visible FIELDs and Values from HashMap startup */
+$startupObj = (object) $dao->getTradeStartuphGetAll();  
+
+/* Get userId responding startup trade */
+$rtn['result'] = array();
+
+foreach ($startupObj as $key => $value) {
+    $v = json_decode($value, true); // $value is encoded in redis
+    $lastTradeNode  = $dao->getTradeOne($key, -1);
+
+    if ($v['visible'] &&
+        strpos($key, $req['userId']) !== false &&
+        strcmp($lastTradeNode['state'], "CLOSE") != 0) { // NOT CLOSE
+
+        $firstTradeNode  = $dao->getTradeOne($key, 0);
+
+        $v['total'] = calPosition($lastTradeNode);
+        $v['pl']    = $v['total'] / calPosition($firstTradeNode) - 1;
+        $rtn['result'][] = array (
+            $key => $v
+        );
+    }
+}
+
+echo json_encode($rtn);
+
+$length = ob_get_length();
+header("Content-Type: application/json; charset=UTF-8");
+header("Content-Length:".$length."\r\n");
+header("Accept-Ranges: bytes"."\r\n");
+
+ob_end_flush();
+
+?>
+
