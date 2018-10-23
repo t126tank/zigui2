0203a6589635755ff13e6262608f3b467866725a
t126tank <t126tank@users.noreply.github.com>
2018-10-23 10:27:54 +0900

    Update temp3.sh

--- tf/sante/python3/temp3.sh
+++ tf/sante/python3/temp3.sh
@@ -9,8 +9,8 @@ pre=($(date "+%Y%m%d-%H%M%S-"))
 
 pushd ~/workspace/bitbucket/nn/workspace/projects/nn
 
-touch $nn
-echo "[]" > $nn
+# touch $nn
+# echo "[]" > $nn
 
 IFS=$'\n' elementes=($(cat 225-list.txt))
 # elementes=($(/usr/bin/php  select_sym_nikkei225.php))
@@ -35,34 +35,50 @@ do
    touch $sym/out/${item}
 #  echo "[{" > $sym/out/${item}
 
-   python3 readCsv3.py $sym
+   if [[ $# -gt 0 ]]; then
+      python3 readCsv3.py $sym all
+   else
+      pushd $sym/out/
+      zip -r $$.zip iris*.csv data.*
+      popd
+
+      python3 readCsv3.py $sym
+   fi
 
    python3 writeCsv3.py $sym > $sym/out/new.txt
-   rm -rf /tmp/iris_model/
+
+   if [[ $# -eq 0 ]]; then
+      pushd $sym/out/
+      unzip -f $$.zip
+      rm -rf *.zip
+      popd
+   fi
+
+#  rm -rf /tmp/iris_model/
 
    python3 tfCsv3.py $sym
 
-   pushd iris
-   cp -f ../$sym/out/iris_test.csv ../$sym/out/iris_training.csv ../$sym/out/input.csv  .
-   env MPLBACKEND=Agg python3 pqs.py $code
+#  pushd iris
+#  cp -f ../$sym/out/iris_test.csv ../$sym/out/iris_training.csv ../$sym/out/input.csv  .
+#  env MPLBACKEND=Agg python3 pqs3.py $code
 
 #  echo "}]" >> ../$sym/out/${item}
 
    # backup model
-#  cp model.npz ../$sym/$pre$code.npz
+#   cp model.npz ../$sym/$pre$code.npz
 
-   popd
+#  popd
 
-   python3 nn3.py $sym
+#  python3 nn3.py $sym
 
    # real-time update, for 1st time
    # cp $nn  $pre$nn
    # sudo cp $nn  $csv /var/www/html/nn/
 done
 
-pre=($(date "+%Y%m%d-%H%M%S-"))
+# pre=($(date "+%Y%m%d-%H%M%S-"))
 
-cp $nn  $pre$nn
+# cp $nn  $pre$nn
 # cp $nn  $csv  /var/www/html/nn/
 
 # rm  backup.zip
@@ -72,4 +88,3 @@ cp $nn  $pre$nn
 # cp -f backup.zip /var/www/html/nn/
 
 popd
-
