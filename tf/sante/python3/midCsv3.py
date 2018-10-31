6407e445053bfaee8005a6eab5d95fd1ee3f6dfe
t126tank <t126tank@users.noreply.github.com>
2018-10-31 17:54:00 +0900

    Update midCsv3.py

--- tf/sante/python3/midCsv3.py
+++ tf/sante/python3/midCsv3.py
@@ -3,55 +3,97 @@
 
 from __future__ import print_function
 
-import sys
-import requests
+import csv
+import datetime
+import os
 import re
+import requests
+import sys
 from bs4 import BeautifulSoup
 
-target_url = 'https://stocks.finance.yahoo.co.jp/stocks/detail/?code=1301'
-try:
-    r = requests.get(target_url)            #requestsを使って、webから取得
-    soup = BeautifulSoup(r.text, 'lxml')    #要素を抽出 (lxml)
+def main(argv):
+    srcDir = "."
 
-    stoksPrice = soup.find('td', class_='stoksPrice')
-    stoksPrice = stoksPrice.find_next_sibling('td').text
-    stoksPrice = float(re.sub('[,]', '', stoksPrice))
-    print(stoksPrice)
+    if len(argv) != 0:
+        srcDir = argv[0]
+        os.chdir(srcDir)
 
-    cnt = 0
-    for dd in soup.find_all('dd', class_='ymuiEditLink mar0'):
-        stng = dd.find('strong').text
-        stng = float(re.sub('[,]', '', stng))
-        print(stng)
-        cnt += 1
-        if cnt > 5:
-            break
+    # コードを取る
+    code = srcDir[7:11]
 
+    # 日付を取る
+    dt = datetime.datetime.today().strftime('%Y-%m-%d')
+    print(dt)
 
-    # Debug
-    '''
-    for td in soup.find_all('td'):
-        print(td.text.encode('cp932', 'ignore'))
-        print(td.get('class'))
-    '''
+    year = datetime.datetime.today().strftime('%Y')
+    print(year)
 
-    #リンクを表示 (lxml)
-    '''
-    for a in soup.find_all('a'):
-        print(a.get('href'))
-    '''
+    csvfile = 'stocks_' + code + '-T_1d_' + year + '.csv'
+    print(csvfile)
 
-except Exception as e:
-    print("error: {0}".format(e), file=sys.stderr)
-    exitCode = 2
+    target_url = 'https://stocks.finance.yahoo.co.jp/stocks/detail/?code=1301'
+    try:
+        r = requests.get(target_url)            #requestsを使って、webから取得
+        soup = BeautifulSoup(r.text, 'lxml')    #要素を抽出 (lxml)
+
+        stoksPrice = soup.find('td', class_='stoksPrice')
+        stoksPrice = stoksPrice.find_next_sibling('td').text
+        stoksPrice = float(re.sub('[,]', '', stoksPrice))
+        print(stoksPrice)
+
+        cnt = 0
+        fields = []
+
+        nums = []
+        for dd in soup.find_all('dd', class_='ymuiEditLink mar0'):
+            stng = dd.find('strong').text
+            stng = float(re.sub('[,]', '', stng))
+            print(stng)
+            nums.append(stng)
+
+            cnt += 1
+            if cnt > 5:
+                break
+
+        fields.append(dt)           # "tradeTime"
+        fields.append(nums[1])      # "o"
+        fields.append(nums[2])      # "h"
+        fields.append(nums[3])      # "l"
+        fields.append(stoksPrice)   # "c"
+        fields.append(nums[4])      # "volume"
+        fields.append(stoksPrice)   # "modified p"
+
+        with open(csvfile, 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(fields)
+
+        # Debug
+        '''
+        for td in soup.find_all('td'):
+            print(td.text.encode('cp932', 'ignore'))
+            print(td.get('class'))
+        '''
+
+        #リンクを表示 (lxml)
+        '''
+        for a in soup.find_all('a'):
+            print(a.get('href'))
+        '''
+
+    except Exception as e:
+        print("error: {0}".format(e), file=sys.stderr)
+        exitCode = 2
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
 ### output
 '''
-3095.0 => 現値
-3005.0 => 前日終値
-3025.0 => 始値
-3105.0 => 高値
-3025.0 => 安値
-26000.0 => 出来高
-79571.0 => 売買代金 (x 1000 千円)
+3095.0 => 現値/close
+3005.0 => 0 前日終値
+3025.0 => 1 始値
+3105.0 => 2 高値
+3025.0 => 3 安値
+26000.0 => 4 出来高
+79571.0 => 5 売買代金 (x 1000 千円)
 '''
