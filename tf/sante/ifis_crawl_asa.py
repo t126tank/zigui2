6e90471fabfebc4ddbecab20f58b2eccc1a5614b
t126tank <t126tank@users.noreply.github.com>
2019-04-12 11:41:15 +0900

    Update ifis_crawl_asa.py

--- tf/sante/ifis_crawl_asa.py
+++ tf/sante/ifis_crawl_asa.py
@@ -29,7 +29,19 @@ def ifisUrl(hc_code):
   arg0 = "index.php?"
   arg1 = "sa=screenRankDetail4csv&"
   arg2 = "ta=p&scid=7"
+
   return BASE_URL + arg0 + arg1 + arg2 + hc_code
+
+def isTarget(r):
+  try:
+    comp = float(r[6])
+    cons = float(r[7])
+
+    return comp > 4.9 or cons > 4.9
+
+  except Exception as e:
+    # print("error: {0}".format(e), file=sys.stderr)
+    return False
 
 
 # "%Y%m%d%H%M%S"
@@ -45,8 +57,22 @@ try:
 
     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
     my_list = list(cr)
+
+    isTitle = True
+    myIndices = [0, 1, 2, 6, 7]
     for row in my_list:
-      json_list.append(row)
+      myRow = []
+      if isTitle or isTarget(row):
+        myRow = [row[i] for i in myIndices]
+        isTitle = False
+
+        if isTarget(row):
+          urlLnk = 'http://stocks.finance.yahoo.co.jp/stocks/detail/?code=' + myRow[0] + '.T&d=6m'
+          myRow.append(urlLnk)
+
+        print(myRow)
+        json_list.append(myRow)
+
 
   # write json
   # f = open(now.strftime("%Y%m%d") + ".json", "w")
@@ -64,3 +90,4 @@ try:
 except Exception as e:
     print("error: {0}".format(e), file=sys.stderr)
     exitCode = 2
+
